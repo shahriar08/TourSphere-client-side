7466931717e70cdbf1f9a9c78c5e093ac35b8d22
AL-Shahriar <anik7744@gmail.com>
2021-11-27 16:09:06 +0600

    change banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,47 +1,28 @@
 import React from 'react';
-import { Carousel } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import './Banner.css'
 
 const Banner = () => {
     return (
-        <div>
-            <Carousel>
-                <Carousel.Item>
+        <div className="container">
+            <div className="row d-flex align-items-top faq mt-5 pt-3">
+                <div className="col-md-6 col-sm-12">
+                    <div className='mx-auto w-50 mt-5'>
+                        <h1 className='fs-1 mt-5'>Find Your Perfect</h1>
+                        <h1 className='fs-1'>Vacation</h1>
+                        <Link to='/event'><Button className='mt-3' variant="danger">Explore More</Button></Link>
+                    </div>
+                </div>
+                <div className="col-md-6 col-sm-12 header-bike  mb-5 d-flex justify-content-center">
                     <img
-                        className="d-block w-100 carousel opacity-75"
-                        src="https://i.ibb.co/HGvzLCB/vestrahorn-mountains-sunset-stokksnes-iceland-2.jpg"
-                        alt="First slide"
+                        src="https://i.ibb.co/vjL9Z42/2507337.jpg"
+                        className="faq-img"
+                        alt="..."
                     />
-                    <Carousel.Caption>
-                        <h1 className='carousel-f1 mx-auto w-50'>Find Your Perfect</h1>
-                        <h1 className='carousel-f2 mx-auto w-50'>Vacation</h1>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100 carousel opacity-75"
-                        src="https://i.ibb.co/cLnqwM6/yosemite-national-park-yosemite-valley-usa-2.jpg"
-                        alt="Second slide"
-                    />
+                </div>
 
-                    <Carousel.Caption>
-                        <h1 className='carousel-f1 mx-auto w-50'>Find Your Perfect</h1>
-                        <h1 className='carousel-f2 mx-auto w-50'>Vacation</h1>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100 carousel opacity-75"
-                        src="https://i.ibb.co/xYVrg5N/landscape-with-mountains-1.jpg"
-                        alt="Third slide"
-                    />
-
-                    <Carousel.Caption className=''>
-                        <h1 className='carousel-f1 mx-auto w-50'>Find Your Perfect</h1>
-                        <h1 className='carousel-f2 mx-auto w-50'>Vacation</h1>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
+            </div>
         </div>
     );
 };
